0b6980b16ad3a1bcdfba60491aba0d44035f4278
aguskrisnaw <guskrisna@gmail.com>
2022-12-22 21:45:53 +0700

    commatcommit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle")
-require('dotenv').config({path:__dirname+'/.env'})
+// require('dotenv').config({path:__dirname+'/.env'})
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
@@ -7,10 +7,10 @@ module.exports = {
       chainId: 1337
     },
 //  unused configuration commented out for now
- mumbai: {
-   url: "https://rpc-mumbai.maticvigil.com",
-   accounts: [process.env.privateKey]
- }
+//  mumbai: {
+//    url: "https://rpc-mumbai.maticvigil.com",
+//    accounts: [process.env.privateKey]
+//  }
   },
   solidity: {
     version: "0.8.4",
